029adbc3f3790cfa63c3309ae3d565d85abcbf1b
Naman9493 <namanchauhan150@gmail.com>
2025-08-23 13:10:40 +0530

    After error correction

--- butterfly.java
+++ butterfly.java
@@ -1,33 +1,32 @@
 import java.util.*;
 public class butterfly {
     public static void Butterfly(int n){
-        for (int i=1;i<=n;i++){
+         for (int i=1;i<=n;i++){
            for ( int j=1;j<=i;j++)
            {
-           System.out.print("* ");  
+           System.out.print("*");  
            }
            for(int j=1;j<=2*(n-i);j++) {
              System.out.print(" ");  
            }
            for(int j=1;j<=i;j++) {
-            System.out.print("* ");  
+            System.out.print("*");  
           }
        System.out.println();  
         }
         
         for (int i=n-1;i>=1;i--){
        
-            for ( int j=1;j<=i;j++)
+            for ( int j=i;j>=1;j--)
             {
-            System.out.print("* ");  
+            System.out.print("*");  
             }
             for(int j=1;j<=2*(n-i);j++) {
               System.out.print(" ");  
             }
-            for(int j=1;j<=i;j++)  {
-             System.out.print("* ");  
+            for(int j=i;j>=1;j--)  {
+             System.out.print("*");  
            }
-       
            System.out.println();  
          }
      } 
